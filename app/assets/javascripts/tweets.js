a236f1ac3cf45be28fb92bf3fdf2b6fb00c89f99
Justin Cheung <justinwkcheung@github.com>
2017-02-21 15:49:36 -0500

    Respond to HTML by updating list of tweets on page rather than redirecting

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,32 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready', function() {
+
+  $('#new_tweet').on('submit', function(event){
+    event.preventDefault();
+    console.log("prevented default");
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize()
+    }).done(function(responseData) {
+      console.log("Came back successfully");
+      console.log(responseData);
+      $('.tweets').prepend(responseData);
+
+    }).fail(function(responseData) {
+      console.log("failed");
+
+
+    }).always(function(responseData) {
+      console.log("this always should happen");
+
+    });
+
+  });
+
+
+
+
+});
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,10 +7,21 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
+    if request.xhr?
+      @tweet.save
+
+      respond_to do |format|
+        format.html { render html: "<li class='tweet'>
+      <p>#{@tweet.message}</p>
+      <time>Feb 21,  8:36 PM</time>
+    </li>".html_safe }
+      end
     else
-      render :index
+      if @tweet.save
+        redirect_to tweets_path
+      else
+        render :index
+      end
     end
   end
 
